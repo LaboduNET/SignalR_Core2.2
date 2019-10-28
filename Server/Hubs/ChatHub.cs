e2b3900d95204029faa6adaf9542bda1407ec4f5
LaboduNET <nbonin@labodunet.fr>
2019-10-28 11:57:05 +0100

    Initial Version

---
+++ Server/Hubs/ChatHub.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Server.Hubs
+{
+	public class ChatHub : Hub
+	{
+
+		private static string PathFile = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		public async Task Join()
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+
+		}
+
+		public async Task Send(string generatedKey, string message)
+		{
+			var generatedKeyList = generatedKey.Split('_');
+			if (generatedKeyList != null && generatedKeyList.Length > 1)
+			{
+				string chatKey = generatedKeyList[0];
+				string userName = generatedKeyList[1];
+				List<ChatMessage> messageList = LoadMessageList(chatKey);
+				var nesMess = new ChatMessage()
+				{
+					Name = userName,
+					Message = message,
+					Date = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+				};
+				messageList.Add(nesMess);
+				SaveMessageListInFile(chatKey, messageList);
+				// Call the addNewMessageToPage method to update clients$.connection.
+				await Clients.Group(chatKey).SendCoreAsync("addNewMessageToPage", new object[] { nesMess });
+			}
+		}
+
+		public async Task InitChat(string generatedKey)
+		{
+			var generatedKeyList = generatedKey.Split('_');
+			if (generatedKeyList != null && generatedKeyList.Length > 1)
+			{
+				string chatKey = generatedKeyList[0];
+				string userName = generatedKeyList[1];
+				await Groups.AddToGroupAsync(Context.ConnectionId, chatKey);
+				List<ChatMessage> messageList = LoadMessageList(chatKey);
+				// Call the addNewMessageToPage method to update clients.
+				await Clients.Caller.SendCoreAsync("loadMessagesToPage", new object[] { messageList });
+			}
+		}
+
+		protected List<ChatMessage> LoadMessageList(string chatKey)
+		{
+			string path = System.IO.Path.Combine(PathFile, $"ChatHubContent_{chatKey}.json");
+			if (System.IO.File.Exists(path))
+			{
+				string fileContent = System.IO.File.ReadAllText(path);
+				var tasksList = JsonConvert.DeserializeObject<List<ChatMessage>>(fileContent);
+
+				return tasksList;
+			}
+			else
+				return new List<ChatMessage>();
+		}
+
+		protected void SaveMessageListInFile(string chatKey, List<ChatMessage> tasksList)
+		{
+			if (tasksList != null)
+			{
+				string json = JsonConvert.SerializeObject(tasksList);
+				json = json.Replace("},{", "},\r\n{").Replace("[{", "[\r\n{").Replace("}]", "}\r\n]");
+
+				string path = System.IO.Path.Combine(PathFile, $"ChatHubContent_{chatKey}.json");
+				System.IO.File.WriteAllText(path, json);
+			}
+		}
+	}
+
+
+	public class ChatMessage
+	{
+		public string Name { get; set; }
+		public string Message { get; set; }
+		public string Date { get; set; }
+	}
+
+
+}
+
---
+++ Server/wwwroot/js/chat.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+//Create the webscket connection
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.ChatKey = "1111_Nico";
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("loadMessagesToPage", function (messages) {
+	messages.map(message => {
+		AddUIMessage(message);
+	});	
+});
+connection.on("addNewMessageToPage", (message) => {
+	// Add the message to the page.
+	AddUIMessage(message);
+	});
+
+
+connection.start().then(function () {
+	document.getElementById("sendButton").disabled = false;
+	connection.invoke("InitChat", connection.ChatKey).catch(function (err) {
+		return console.error(err.toString());
+	});
+}).catch(function (err) {
+	return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+	var message = document.getElementById("messageInput").value;
+	connection.invoke("Send", connection.ChatKey, message).catch(function (err) {
+		return console.error(err.toString());
+	});
+	event.preventDefault();
+});
+
+function AddUIMessage(message) {
+	var encodedMsg = '<strong>' +
+		htmlEncode(message.name) +
+		'</strong>: (' + htmlEncode(message.date) + ') <br/>' +
+		htmlEncode(message.message).split('\n').join('<br/>'); 	
+	var li = document.createElement("li");
+	li.innerHTML = encodedMsg;
+	document.getElementById("messagesList").appendChild(li);
+}
+// This optional function html-encodes messages for display in the page.
+function htmlEncode(value) {
+	var encodedValue = $('<div />').text(value).html();
+	return encodedValue;
+}
